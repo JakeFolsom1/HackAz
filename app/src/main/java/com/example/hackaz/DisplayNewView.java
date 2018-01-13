812db297d16522ff4024bdb6a7e33dd25f92d255
Jake Folsom <jfolsom2@asu.edu>
2018-01-13 08:56:57 -0700

    Added new pane selection and routing to new pane

--- app/src/main/java/com/example/hackaz/DisplayNewView.java
+++ app/src/main/java/com/example/hackaz/DisplayNewView.java
@@ -8,6 +8,5 @@ public class DisplayNewView extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_new_view);
     }
 }
