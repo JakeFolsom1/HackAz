c1cc1cc96e79092826bc23e435df4aefb1755d43
dunchuan <wudunchuan@gmail.com>
2018-01-13 12:33:04 -0700

    Added routing to the buttons on ingredients page

--- app/src/main/java/com/example/hackaz/DisplayNewView.java
+++ app/src/main/java/com/example/hackaz/DisplayNewView.java
@@ -1,12 +1,20 @@
 package com.example.hackaz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
 public class DisplayNewView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+    public void BackToMenu(View view) {
+        Intent openNewActivity = new Intent(this, HomePage.class);
+        startActivity(openNewActivity);
+
     }
 }
