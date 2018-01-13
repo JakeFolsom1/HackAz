d66b5fa97714e336b6233ed5022754a08dbe0559
Jake Folsom <jfolsom2@asu.edu>
2018-01-13 03:36:13 -0700

    UI fixes

---
+++ app/src/main/java/com/example/hackaz/DisplayNewView.java
@@ -0,0 +1,13 @@
+package com.example.hackaz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayNewView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_new_view);
+    }
+}
